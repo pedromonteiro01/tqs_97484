6e9905be5fd17f6b93b8624c22329c77ae66a5ab
pedromonteiro01 <pmapm@ua.pt>
2022-03-18 15:21:05 +0000

    add hamcrest library

--- lab2/lab2_1/tqs_lab2/src/test/java/com/tqs_lab2/StocksPortfolioTest.java
+++ lab2/lab2_1/tqs_lab2/src/test/java/com/tqs_lab2/StocksPortfolioTest.java
@@ -9,6 +9,9 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import static org.mockito.Mockito.*;
 
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.core.Is.is;
+
 @ExtendWith(MockitoExtension.class)
 class StocksPortfolioTest {
 
@@ -32,6 +35,23 @@ class StocksPortfolioTest {
         double result = portfolio.getTotalValue();
 
         assertEquals(5, result);
+        verify(stockService, times(2)).lookUpPrice(anyString());
+    }
+
+    @Test
+    void testGetTotalValueHamcrest(){
+        Stock stock1 = new Stock("STOCK1", 1);
+        Stock stock2 = new Stock("STOCK2", 2);
+
+        portfolio.addStock(stock1);
+        portfolio.addStock(stock2);
+        
+        when(stockService.lookUpPrice("STOCK1")).thenReturn(1.0);
+        when(stockService.lookUpPrice("STOCK2")).thenReturn(2.0);
+
+        double result = portfolio.getTotalValue();
+
+        assertThat(result, is(5));
         verify(stockService, times(2)).lookUpPrice(anyString());
     }
 
