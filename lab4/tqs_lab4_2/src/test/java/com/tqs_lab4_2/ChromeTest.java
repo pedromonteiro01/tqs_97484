e97093d5dcdb8584b37bbfc1b5534d722bb1c1dc
pedromonteiro01 <pmapm@ua.pt>
2022-04-06 13:59:06 +0100

    lab4_2

--- lab4/tqs_lab4_2/src/test/java/com/tqs_lab4_2/ChromeTest.java
+++ lab4/tqs_lab4_2/src/test/java/com/tqs_lab4_2/ChromeTest.java
@@ -3,16 +3,20 @@ package com.tqs_lab4_2;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+//import io.github.bonigarcia.seljup.EnabledIfBrowserAvailable;
+import io.github.bonigarcia.seljup.SeleniumJupiter;
 
 import static org.assertj.core.api.Assertions.*;
 
 /**
  * Unit test for simple App.
  */
-class AppTest {
+@ExtendWith(SeleniumJupiter.class)
+class ChromeTest {
     /**
      * Rigorous Test.
      */
@@ -42,5 +46,11 @@ class AppTest {
     @AfterEach
     void teardown() {
         driver.quit();
+    }
+
+    @Test
+    void test(ChromeDriver driver) {
+        driver.get("https://blazedemo.com/");
+        assertThat(driver.getTitle()).contains("BlazeDemo");
     }
 }
