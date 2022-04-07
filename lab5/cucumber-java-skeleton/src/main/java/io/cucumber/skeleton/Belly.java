9f716df8596c7cbfdb279bbe52667be6cfeada87
pedromonteiro01 <pmapm@ua.pt>
2022-04-07 14:54:26 +0100

    lab5_1

---
+++ lab5/cucumber-java-skeleton/src/main/java/io/cucumber/skeleton/Belly.java
@@ -0,0 +1,15 @@
+package io.cucumber.skeleton;
+
+public class Belly {
+    public void eat(int cukes) {
+
+    }
+
+    public void wait(int min) {
+
+    }
+
+    public void growl() {
+        
+    }
+}
---
+++ lab5/cucumber-java-skeleton/src/test/java/io/cucumber/skeleton/StepDefinitions.java
@@ -0,0 +1,53 @@
+package io.cucumber.skeleton;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDefinitions {
+    @Given("I have {int} cukes in my belly")
+    public void I_have_cukes_in_my_belly(int cukes) {
+        Belly belly = new Belly();
+        belly.eat(cukes);
+    }
+
+    @When("I wait {int} hour")
+    public void wait_time(Integer min) {
+        Belly belly = new Belly();
+        belly.wait(min);
+    }
+
+    @Then("my belly should growl")
+    public void my_belly_should_growl() {
+        Belly belly = new Belly();
+        belly.growl();
+    }
+
+    private Calculator calc;
+
+    @Given("a calculator I just turned on")
+    public void setup() {
+        calc = new Calculator();
+    }
+
+    @When("I add {int} and {int}")
+    public void add(int arg1, int arg2) {
+        calc.push(arg1);
+        calc.push(arg2);
+        calc.push("+");
+    }
+
+    @When("I substract {int} to {int}")
+    public void substract(int arg1, int arg2) {
+        calc.push(arg1);
+        calc.push(arg2);
+        calc.push("-");
+    }
+
+    @Then("the result is {int}")
+    public void the_result_is(double expected) {
+        assertEquals(expected, calc.value());
+    }
+}
