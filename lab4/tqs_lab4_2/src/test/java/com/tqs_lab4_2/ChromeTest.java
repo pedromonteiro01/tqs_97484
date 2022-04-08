13a81b153c22fa58699090edfb75aae437895b37
pedromonteiro01 <pmapm@ua.pt>
2022-04-08 12:05:12 +0100

    update lab4_2

--- lab4/tqs_lab4_2/src/test/java/com/tqs_lab4_2/ChromeTest.java
+++ lab4/tqs_lab4_2/src/test/java/com/tqs_lab4_2/ChromeTest.java
@@ -4,7 +4,10 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 //import io.github.bonigarcia.seljup.EnabledIfBrowserAvailable;
@@ -52,5 +55,40 @@ class ChromeTest {
     void test(ChromeDriver driver) {
         driver.get("https://blazedemo.com/");
         assertThat(driver.getTitle()).contains("BlazeDemo");
+    }
+
+    @Test
+    public void flightsTest(WebDriver driver) {
+      driver.get("https://blazedemo.com/");
+      driver.manage().window().setSize(new Dimension(1920, 1080));
+      driver.findElement(By.name("fromPort")).click();
+      {
+        WebElement dropdown = driver.findElement(By.name("fromPort"));
+        dropdown.findElement(By.xpath("//option[. = 'New York']")).click();
+        dropdown.findElement(By.xpath("//option[. = 'Boston']")).click();
+      }
+      driver.findElement(By.name("toPort")).click();
+      {
+        WebElement dropdown = driver.findElement(By.name("toPort"));
+        dropdown.findElement(By.xpath("//option[. = 'New York']")).click();
+      }
+        driver.findElement(By.cssSelector(".btn-primary")).click();
+        driver.findElement(By.cssSelector("tr:nth-child(3) .btn")).click();
+        driver.findElement(By.id("inputName")).click();
+        driver.findElement(By.id("inputName")).sendKeys("Pedro");
+        driver.findElement(By.cssSelector(".control-group:nth-child(2) > .controls")).click();
+        driver.findElement(By.id("address")).click();
+        driver.findElement(By.id("address")).sendKeys("Guarda");
+        driver.findElement(By.cssSelector(".control-group:nth-child(2) > .controls")).click();
+        driver.findElement(By.id("city")).click();
+        driver.findElement(By.id("city")).sendKeys("Guarda");
+        driver.findElement(By.cssSelector(".control-group:nth-child(2) > .controls")).click();
+        driver.findElement(By.id("zipCode")).click();
+        driver.findElement(By.id("zipCode")).sendKeys("6300-255");
+        driver.findElement(By.cssSelector(".control-group:nth-child(2) > .controls")).click();
+        driver.findElement(By.id("state")).click();
+        driver.findElement(By.id("state")).sendKeys("idk");
+        driver.findElement(By.cssSelector("form")).click();
+        driver.findElement(By.cssSelector(".btn-primary")).click();
     }
 }
